3602415a36a217b9e80355550189596c1078c358
qxqzx3489 <qxqzx@arch.localdomain>
2023-10-10 09:31:27 +0800

    添加编译win64

---
+++ scripts/mingw-w64.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+# Arch build
+echo "请全部安装!"
+paru -S mingw-w64
+
+export CC=x86_64-w64-mingw32-gcc
+export CXX=x86_64-w64-mingw32-g++
+
+if [ -f "go.mod" ]; then
+  GOARCH=amd64 CGO_ENABLED=1 GOOS=windows go build -o webshellManager.exe
+else
+  cd ../;GOARCH=amd64 CGO_ENABLED=1 GOOS=windows go build -o webshellManager.exe
+fi
+
+
+echo "编译完成!"
